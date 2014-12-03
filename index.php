5128d8bb5e2185c6e2b82ed712fa71e5338cb577
metaksu <met-aksu@hotmail.com>
2014-12-03 18:41:25 -0500

    version 0.1.2 - travel site

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html>
-<title>Vile Studio</title>
-<link rel="stylesheet" type="text/css" href="styles.css"/>
-<head>
-</head>
 <body>
 	<div class="nav">
 		<div class="container">
@@ -19,30 +15,32 @@
 		</div>
 	</div>
 	<div class="jumbotron">
-		<div class="learn more">
+		<div class="learn-more">
 			<div class="container">
-				<h3>Travel</h3>
-				<p>From apartments and rooms to treehouses and boats: stay in unique space in 192 countries</p>
-				<p><a href="#">See how to travel on Airbnb</a></p>
-				<div class="container">
-					<h1>Find a nice place to stay.</h1>
-					<p>Rent from people in over 34,000 cities and 192 countries</p>
-				</div>
-			</div>
-			<div class="container">
-				<h3>Host</h3>
-				<p>Renting out your unused space could pay your bills or find your next vacation</p>
-				<p>
-					<a href="#">Learn more about hosting</a>
-				</p>
-			</div>
-			<div class="container">
-				<h3>Trust and Safety</h3>
-				<p>From Verified ID to our worldwide customer support team, we've got your back."</p>
-				<p>
-					<a href="#">Learn about trust at Airbnb</a>
-				</p>
-			</div>
+    			<div>
+    				<h3>Travel</h3>
+    				<p>From apartments and rooms to treehouses and boats: stay in unique space in 192 countries</p>
+    				<p><a href="#">See how to travel on Airbnb</a></p>
+    				<div>
+    					<h1>Find a nice place to stay.</h1>
+    					<p>Rent from people in over 34,000 cities and 192 countries</p>
+    				</div>
+    			</div>
+    			<div>
+    				<h3>Host</h3>
+    				<p>Renting out your unused space could pay your bills or find your next vacation</p>
+    				<p>
+    					<a href="#">Learn more about hosting</a>
+    				</p>
+    			</div>
+    			<div>
+    				<h3>Trust and Safety</h3>
+    				<p>From Verified ID to our worldwide customer support team, we've got your back."</p>
+    				<p>
+    					<a href="#">Learn about trust at Airbnb</a>
+    				</p>
+    			</div>
+    		</div>
 		</div>
 	</div>
 </body>

5a46400c879e77fadba6e5b25a6db12067f01ae7
metaksu <met-aksu@hotmail.com>
2014-12-03 18:36:05 -0500

    version 0.1.1 - travel site

--- index.php
+++ index.php
@@ -1,26 +1,49 @@
 <!DOCTYPE html>
 <html>
-	<title>Vile Studios</title>
-	<link rel="stylesheet" type="text/css" href="styles.css"/>
-
+<title>Vile Studio</title>
+<link rel="stylesheet" type="text/css" href="styles.css"/>
 <head>
-	<p>
-		Buttholes
-		Mike loves men!
-	</p>
 </head>
-	<body>
-		<div class="header">
-			<p></p>
-		</div>
-		<div id="list">
+<body>
+	<div class="nav">
+		<div class="container">
 			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>Metin</li>
+				<li>Browse</li>
+			</ul>
+			<ul>
+				<li>Sign up</li>
+				<li>Log In</li>
+				<li>Help</li>
 			</ul>
 		</div>
-	<body/>
+	</div>
+	<div class="jumbotron">
+		<div class="learn more">
+			<div class="container">
+				<h3>Travel</h3>
+				<p>From apartments and rooms to treehouses and boats: stay in unique space in 192 countries</p>
+				<p><a href="#">See how to travel on Airbnb</a></p>
+				<div class="container">
+					<h1>Find a nice place to stay.</h1>
+					<p>Rent from people in over 34,000 cities and 192 countries</p>
+				</div>
+			</div>
+			<div class="container">
+				<h3>Host</h3>
+				<p>Renting out your unused space could pay your bills or find your next vacation</p>
+				<p>
+					<a href="#">Learn more about hosting</a>
+				</p>
+			</div>
+			<div class="container">
+				<h3>Trust and Safety</h3>
+				<p>From Verified ID to our worldwide customer support team, we've got your back."</p>
+				<p>
+					<a href="#">Learn about trust at Airbnb</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</body>
 </html>

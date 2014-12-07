8086de58ce1a69cb26a32499fba6a2e98ac2991a
metaksu <met-aksu@hotmail.com>
2014-12-07 17:28:17 -0500

    working

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
 	}, function() {
 		$(this).css('background-color', '#4B4B4B');
 	});
+
+	$('.icon-menu').click(function() {
+		$('.menu').animate({left: '0px'}, 200);
+		$('body').animate({left: '285px'}, 200);
+	});
+	$('.icon-close').click(function() {
+		$('.menu').animate({left: '-285px'}, 200);
+		$('body').animate({left: '0px'}, 200);
+	});
 });

57c8d1a795eabe3fd805c60667c91dc12223875d
metaksu <met-aksu@hotmail.com>
2014-12-07 16:59:39 -0500

    Sharing buttons started 0.1.0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+	$('.button').hover(function() {
+		$(this).css('background-color', '#B7B7B7');
+	}, function() {
+		$(this).css('background-color', '#4B4B4B');
+	});
+});
